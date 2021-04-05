0709771f1464fc298bd560b670bee7645f992781
heeyoung <68907892+heeyoung2670@users.noreply.github.com>
2021-04-05 19:44:30 +0900

    Delete LISTA_test/LISTA_test directory

--- LISTA_test/LISTA_test/tools/data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import math
-
-def bernoulli_gaussian_trial(A,M,N,B,pnz,SNR):
-    noise_var = pnz*N/M * math.pow(10., -SNR / 10.)
-    xval = ((np.random.uniform( 0,1,(N,B))<pnz) * np.random.normal(0,1,(N,B))).astype(np.float32)
-    yval = np.matmul(A,xval) + np.random.normal(0,math.sqrt( noise_var ),(M,B))
-    return xval,yval
